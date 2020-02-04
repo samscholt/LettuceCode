d03fa26d6427c34fbed526e22fd6505dffd9d20f
Michael Vanderloon <jenkins92886@gmail.com>
2020-02-04 04:55:53 -0500

    worked on api logic

--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service'
 
 @Component({
   selector: 'app-search-criteria',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchCriteriaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Recipe : RecipeService) { }
+userInput: string;
+recipes:[]
 
   ngOnInit() {
-  }
 
+    this.Recipe.getRecipes(this.userInput).subscribe( (response: any) => this.recipes= response.hits.recipe);
+  }
 }
