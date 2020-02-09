d9b024d374782d337fb703f05b7e1cf8b6d4e4ed
jch2617 <jch2617@gmail.com>
2020-02-08 21:18:21 -0500

    added loading prompt

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -13,17 +13,17 @@ export class RecipeListComponent implements OnInit {
   constructor(private search : SearchCriteriaComponent, private Recipe : RecipeService) { }
   userInput : any;
   recipes=[];
-  searching: boolean = false;
    
   ngOnInit() {
   }
+
+  searching: boolean = false;
 
   onSubmit({userInput, calories,vegetarian,vegan, treenuts}){
     this.searching = true;
     return this.Recipe.getRecipes(userInput,calories,vegetarian,vegan, treenuts).subscribe(
     ((data:any) => this.recipes = data.hits),
     (() => this.searching = false));
-
   }
 
   addFavorite(yummy) {
